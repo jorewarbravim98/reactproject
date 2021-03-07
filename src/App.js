c2f588a6de2672822bc63211cb4a98dd3d2ec04d
bravim <jorewarbravim98>
2021-03-07 17:18:58 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
-     <h1>React Project Intiated</h1>
-     <h1>React Project Intiated with all code</h1>
+  
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//import './App.css';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+const name="Bravim";
+//const currDate=new Date().toLocaleDateString();
+//const currTime=new Date().toLocaleTimeString();
+const img1="https://picsum.photos/200/300";
+const img2="https://picsum.photos/250/300";
+const img3="https://picsum.photos/300/300";
+const link1="https://www.youtube.com/watch?v=OloAP8p1k1w&list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ&index=14"
+const heading={
+    color: "brown",
+    textAlign: "center",
+    textTransform: "capitalize",
+    fontWeight: "bold",
+    textShadow:"0px 2px 4px #ffe9c5",
+    margin: "70px"
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+  <h1 style={heading}>Hello, My name is {name}</h1>
+  <div className="img_div">
+  <a href={link1} target="_youtube">
+  <img src={img1} alt="nature"/>
+  <img src={img2} alt="nature"/>
+  <img src={img3} alt="nature"/>
+  </a>
+  </div>
+  </>,
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+//import "./index.css";
+import Greet from './Greet';
+ReactDOM.render(<Greet/>,document.getElementById("root"));
