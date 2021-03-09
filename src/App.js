69d267849a2fe4f5cbd25a3ab6b21f4c013ef0a5
bravim <jorewarbravim98>
2021-03-09 14:37:39 +0530

    App update with fun

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-//import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+//import './App.css';
+import {add,sub,mul,div} from './Calc'
 function App() {
   return (
-    <div className="App">
-  
+    <div >
+  <ul>
+    <li>Sum is {add(40,4)}</li>
+    <li>Sub is {sub(40,4)}</li>
+    <li>Mul is {mul(40,4)}</li>
+    <li>Div is {div(40,4)}</li>
+  </ul>
     </div>
   );
 }
