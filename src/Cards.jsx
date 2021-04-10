8c86234ab4d4afe8e2449bea296a4550d4dc8d51
jorewarbravim98 <bravimjorewar98@gmail.com>
2021-04-10 09:19:36 +0530

    target blank

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -8,7 +8,7 @@ function Cards(props){
           <div className="card_info">
             <span className="card_category"> {props.title}</span>
             <h3 className="card_title"> {props.Sname}</h3>
-            <a href={props.link} >
+            <a href={props.link } target="_blank" >
               <button>Watch Now</button>
             </a>
           </div>
